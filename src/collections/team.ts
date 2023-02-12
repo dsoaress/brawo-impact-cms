2849dd6e9d19d7b19074e894a49d2e13ce5a64d4
Daniel Soares <daniel.soares@me.com>
2023-02-12 12:01:03 -0300

    feat: add team and media (#2)

---
+++ src/collections/team.ts
@@ -0,0 +1,57 @@
+import type { CollectionConfig } from 'payload/types'
+
+export const Team: CollectionConfig = {
+  slug: 'team',
+  admin: {
+    useAsTitle: 'name',
+    description: "Membres de l'équipe"
+  },
+  labels: {
+    plural: 'Équipe',
+    singular: "membre de l'équipe"
+  },
+  access: {
+    read: () => true
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+      label: 'Nom',
+      admin: {
+        description: "Nom du membre de l'équipe",
+        placeholder: 'Exemple: John Doe'
+      }
+    },
+    {
+      name: 'role',
+      type: 'text',
+      required: true,
+      label: 'Fonction',
+      admin: {
+        description: "Fonction du membre de l'équipe",
+        placeholder: 'Exemple: Développeur'
+      }
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      required: true,
+      label: 'Image',
+      relationTo: 'media',
+      admin: {
+        description: "Image du membre de l'équipe"
+      }
+    },
+    {
+      name: 'order',
+      type: 'number',
+      required: true,
+      label: 'Commande',
+      admin: {
+        description: "Ordre d'affichage du membre de l'équipe"
+      }
+    }
+  ]
+}
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,20 @@ import 'dotenv/config'
 
 import express from 'express'
 import payload from 'payload'
+import { mediaManagement } from 'payload-cloudinary-plugin'
 
 const app = express()
 
+app.use(
+  mediaManagement(
+    {
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET
+    },
+    { folder: process.env.CLOUDINARY_FOLDER }
+  )
+)
 app.get('/', (_, res) => res.redirect('/app'))
 
 const start = async () => {
